48843fc0cf77899292494babd6b093a51701dce9
ChsHub <christian1193@web.de>
2019-11-16 16:11:04 +0100

    rename;

--- setup_cython_test.py
+++ setup_cython_test.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("delete_dublicates.py")
+    ext_modules=cythonize("delete_duplicates.py")
 )
  # python setup_cython_test.py build_ext --inplace
